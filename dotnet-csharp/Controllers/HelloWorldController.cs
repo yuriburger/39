19992a2fb9721cb46c90c42aee5f134ac00a6c31
Yuri Burger <yburger@vxcompany.com>
2020-07-01 23:06:55 +0200

    Added dotnetcore

---
+++ dotnet-csharp/Controllers/HelloWorldController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_csharp.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class HelloWorldController : ControllerBase
+    {
+        [HttpGet]
+        public String Get()
+        {
+            return "Hello World!";
+        }
+    }
+}
